2461a5e7257daa5855fc12633aa29f2e9f90b2cb
Kirill Konovalov <auddax@gmail.com>
2022-05-07 14:21:24 +0300

    feat: add keyboard handlers

--- src/script.js
+++ src/script.js
@@ -1,1 +1,62 @@
+const textarea = document.querySelector('.keyboard-output');
+const keyboard = document.querySelector('.keyboard');
 
+/* Keyboard Handlers */
+
+function onKeyDown(event) {
+  const keys = document.querySelectorAll('.key');
+  keys.forEach((item) => {
+    if (item.innerText.toUpperCase() === event.key.toUpperCase()) {
+      item.classList.add('key_pressed');
+    }
+  });
+
+  const targetElement = document.getElementById(event.code.toLowerCase());
+  if (targetElement) {
+    targetElement.classList.add('key_pressed');
+
+    // Handle Tab button
+    if (targetElement.id === 'tab') {
+      event.preventDefault();
+      textarea.value += '    ';
+    }
+  }
+}
+
+function onKeyUp(event) {
+  const keys = document.querySelectorAll('.key');
+  keys.forEach((item) => {
+    if (item.innerText.toUpperCase() === event.key.toUpperCase()) {
+      item.classList.remove('key_pressed');
+    }
+  });
+
+  const targetElement = document.getElementById(event.code.toLowerCase());
+  if (targetElement) targetElement.classList.remove('key_pressed');
+}
+
+document.addEventListener('keydown', onKeyDown);
+document.addEventListener('keyup', onKeyUp);
+
+/* Mouse Handlers */
+
+function onMouseDown(event) {
+  if (event.target.classList.contains('key')) {
+    event.target.classList.add('key_pressed');
+    if (!event.target.classList.contains('key_text')) {
+      textarea.value += event.target.innerText.toLowerCase();
+    } else {
+      console.log(event.target.id);
+    }
+  }
+}
+
+function onMouseUp(event) {
+  if (event.target.classList.contains('key')) {
+    event.target.classList.remove('key_pressed');
+    textarea.focus();
+  }
+}
+
+keyboard.addEventListener('mousedown', onMouseDown);
+keyboard.addEventListener('mouseup', onMouseUp);
